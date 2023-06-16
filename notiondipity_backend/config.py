b05d657bc5b08739fba948f4bd4e2c77beb71b65
Milos Svana <milos.svana@gmail.com>
2023-06-16 15:40:14 +0200

    Removed unneeded config and  module

--- notiondipity_backend/config.py
+++ notiondipity_backend/config.py
@@ -4,10 +4,5 @@ NOTION_BASE_URL = 'https://api.notion.com/v1/'
 NOTION_OAUTH_CLIENT_ID = os.environ.get('NOTION_OAUTH_CLIENT_ID')
 NOTION_OAUTH_CLIENT_SECRET = os.environ.get('NOTION_OAUTH_CLIENT_SECRET')
 
-NOTION_HEADERS = {
-    "Notion-Version": "2022-06-28",
-    "Content-Type": "application/json",
-}
-
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 JWT_SECRET = os.environ.get('JWT_SECRET')
