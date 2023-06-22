7fbf225b07d63b94e14610e69779a61e335b1c68
Milos Svana <milos.svana@mailfence.com>
2023-06-22 15:49:45 +0200

    Switched to a different test user

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -14,7 +14,7 @@ def test_not_has_data(client):
 
 def test_has_data(client, db):
     cursor = db.cursor()
-    cursor.execute("INSERT INTO last_updates VALUES ('b18801e0-dc26-4fbf-8fdf-963d25f44590', '2023-06-19 10:38:23', 1)")
+    cursor.execute("INSERT INTO last_updates VALUES ('48c73c5c-0d53-4383-83e8-2ef1bfbebe4e', '2023-06-19 10:38:23', 1)")
     db.commit()
     response = client.get('/has-data', headers=TEST_HEADERS)
     assert response.status_code == 200
@@ -25,7 +25,7 @@ def test_has_data(client, db):
 def test_refresh_too_soon(client, db):
     cursor = db.cursor()
     now = datetime.now()
-    cursor.execute("INSERT INTO last_updates VALUES ('b18801e0-dc26-4fbf-8fdf-963d25f44590', %s, 1)", [now])
+    cursor.execute("INSERT INTO last_updates VALUES ('48c73c5c-0d53-4383-83e8-2ef1bfbebe4e', %s, 1)", [now])
     db.commit()
     response = client.get('/refresh-embeddings', headers=TEST_HEADERS)
     assert response.status_code == 425
@@ -37,7 +37,6 @@ def test_resfresh(client, db):
     response = client.get('/refresh-embeddings', headers=TEST_HEADERS)
     assert response.status_code == 200
     assert response.json['status'] == 'OK'
-    cursor.execute(
-        'SELECT COUNT(*) AS cnt FROM embeddings WHERE user_id = %s', ['b18801e0-dc26-4fbf-8fdf-963d25f44590'])
+    cursor.execute("SELECT COUNT(*) AS cnt FROM embeddings WHERE user_id = '48c73c5c-0d53-4383-83e8-2ef1bfbebe4e'")
     num_rows = cursor.fetchone()[0]
-    assert num_rows > 50
+    assert num_rows == 7
