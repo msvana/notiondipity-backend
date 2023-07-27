53ad1e6a53f8b74e4b09db7cff112a929a2888bb
Milos Svana <milos.svana@mailfence.com>
2023-07-27 09:27:00 +0200

    Fix 'Too many tokens' error by limiting the number of tokens to 2800

--- notiondipity_backend/resources/gpt.py
+++ notiondipity_backend/resources/gpt.py
@@ -1,6 +1,8 @@
 import json
+import logging
 
 import openai
+import tiktoken
 
 from notiondipity_backend.config import OPENAI_API_KEY
 
@@ -9,7 +11,8 @@ openai.api_key = OPENAI_API_KEY
 
 def get_ideas(pages: list[str]) -> list[dict]:
     prompt = ['''
-        Generate a list of project idea suggestions from the following Notion pages for further processing. 
+        Generate a list of project idea suggestions from the following Notion pages and then process them 
+        for further use. Business ideas should be preferred, but other project ideas are welcome too. 
         Utilize non-obvious hidden connection between the pages. Add a short description for each idea:\n''']
 
     function_definition = {
@@ -42,10 +45,18 @@ def get_ideas(pages: list[str]) -> list[dict]:
     for i, page in enumerate(pages):
         prompt.append(f'PAGE {i + 1}:\n------\n{page}\n')
     prompt = '\n'.join(prompt)
+    token_encoder = tiktoken.get_encoding('cl100k_base')
+    prompt_encoded = token_encoder.encode(prompt)
+    prompt = token_encoder.decode(prompt_encoded[:2800])
     completion = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': prompt}], functions=[function_definition])
+        model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': prompt}],
+        functions=[function_definition], temperature=0.2)
     response = completion.choices[0].message
     if 'function_call' in response:
-        return json.loads(response['function_call']['arguments'])['ideas']
+        try:
+            return json.loads(response['function_call']['arguments'])['ideas']
+        except json.decoder.JSONDecodeError:
+            logging.error(f"Could not decode string: {response['function_call']['arguments']}")
+            return []
     else:
         return []
