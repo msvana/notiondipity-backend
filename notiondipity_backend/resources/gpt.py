46f27f25a93404848841a63ef8e5f65e861976d7
Milos Svana <milos.svana@mailfence.com>
2023-08-16 09:35:36 +0200

    Refactor idea generation by separating page comparison into a standalone function

--- notiondipity_backend/resources/gpt.py
+++ notiondipity_backend/resources/gpt.py
@@ -10,15 +10,11 @@ openai.api_key = OPENAI_API_KEY
 
 
 def get_ideas(pages: list[str]) -> list[dict]:
-    prompt_compare_pages = ['''
-        Compare the contents of these notion pages. What are the similarities in the ideas. How do they differ? 
-        Can ideas from different pages be somehow combined?\n''']
-
     prompt = '''
         Generate a list of project idea suggestions from the following Notion pages and then process them 
         for further use. Business ideas should be preferred, but other project ideas are welcome too. 
         Utilize non-obvious hidden connections between different pages. Add a short description for each idea.
-        Be careful not to use the " symbol when writing description. '''
+        Be careful not to use the " symbol when writing description.'''
 
     function_definition = {
         'name': 'process_project_ideas',
@@ -47,18 +43,7 @@ def get_ideas(pages: list[str]) -> list[dict]:
         }
     }
 
-    for i, page in enumerate(pages):
-        prompt_compare_pages.append(f'------\nPAGE {i + 1}:\n------\n{page}\n')
-        prompt_compare_pages.append('------ END ------')
-
-    prompt_compare_pages = '\n'.join(prompt_compare_pages)
-    token_encoder = tiktoken.get_encoding('cl100k_base')
-    prompt_compare_pages_encoded = token_encoder.encode(prompt_compare_pages)
-    prompt_compare_pages = token_encoder.decode(prompt_compare_pages_encoded[:5000])
-
-    messages = [{'role': 'user', 'content': prompt_compare_pages}]
-    completion = openai.ChatCompletion.create(model='gpt-3.5-turbo-16k', messages=messages, temperature=0.2)
-    messages.append(completion.choices[0].message)
+    messages = compare_pages(pages)
     messages.append({'role': 'user', 'content': prompt})
     completion = openai.ChatCompletion.create(
         model='gpt-3.5-turbo-16k', messages=messages,
@@ -74,3 +59,23 @@ def get_ideas(pages: list[str]) -> list[dict]:
             return []
     else:
         return []
+
+
+def compare_pages(pages: list[str]) -> list[dict]:
+    prompt = ['''
+        Compare the contents of these notion pages. What are the similarities in the ideas. How do they differ? 
+        Can ideas from different pages be somehow combined?\n''']
+
+    for i, page in enumerate(pages):
+        prompt.append(f'------\nPAGE {i + 1}:\n------\n{page}\n')
+        prompt.append('------ END ------')
+
+    prompt = '\n'.join(prompt)
+    token_encoder = tiktoken.get_encoding('cl100k_base')
+    prompt_encoded = token_encoder.encode(prompt)
+    prompt = token_encoder.decode(prompt_encoded[:5000])
+
+    messages = [{'role': 'user', 'content': prompt}]
+    completion = openai.ChatCompletion.create(model='gpt-3.5-turbo-16k', messages=messages, temperature=0.2)
+    messages.append(completion.choices[0].message)
+    return messages
