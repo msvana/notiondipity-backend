77e222f1259ab50cd9294e83999d80655637eee7
Milos Svana <milos.svana@mailfence.com>
2023-08-16 09:30:05 +0200

    Improve idea generation by explicitly asking GPT to compare the contents of considered pages

--- notiondipity_backend/resources/gpt.py
+++ notiondipity_backend/resources/gpt.py
@@ -10,10 +10,15 @@ openai.api_key = OPENAI_API_KEY
 
 
 def get_ideas(pages: list[str]) -> list[dict]:
-    prompt = ['''
+    prompt_compare_pages = ['''
+        Compare the contents of these notion pages. What are the similarities in the ideas. How do they differ? 
+        Can ideas from different pages be somehow combined?\n''']
+
+    prompt = '''
         Generate a list of project idea suggestions from the following Notion pages and then process them 
         for further use. Business ideas should be preferred, but other project ideas are welcome too. 
-        Utilize non-obvious hidden connections between different pages. Add a short description for each idea:\n''']
+        Utilize non-obvious hidden connections between different pages. Add a short description for each idea.
+        Be careful not to use the " symbol when writing description. '''
 
     function_definition = {
         'name': 'process_project_ideas',
@@ -43,15 +48,24 @@ def get_ideas(pages: list[str]) -> list[dict]:
     }
 
     for i, page in enumerate(pages):
-        prompt.append(f'PAGE {i + 1}:\n------\n{page}\n')
-    prompt = '\n'.join(prompt)
+        prompt_compare_pages.append(f'------\nPAGE {i + 1}:\n------\n{page}\n')
+        prompt_compare_pages.append('------ END ------')
+
+    prompt_compare_pages = '\n'.join(prompt_compare_pages)
     token_encoder = tiktoken.get_encoding('cl100k_base')
-    prompt_encoded = token_encoder.encode(prompt)
-    prompt = token_encoder.decode(prompt_encoded[:2800])
+    prompt_compare_pages_encoded = token_encoder.encode(prompt_compare_pages)
+    prompt_compare_pages = token_encoder.decode(prompt_compare_pages_encoded[:5000])
+
+    messages = [{'role': 'user', 'content': prompt_compare_pages}]
+    completion = openai.ChatCompletion.create(model='gpt-3.5-turbo-16k', messages=messages, temperature=0.2)
+    messages.append(completion.choices[0].message)
+    messages.append({'role': 'user', 'content': prompt})
     completion = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo', messages=[{'role': 'user', 'content': prompt}],
-        functions=[function_definition], temperature=0.2)
+        model='gpt-3.5-turbo-16k', messages=messages,
+        functions=[function_definition], temperature=0.2,
+        function_call={"name": "process_project_ideas"})
     response = completion.choices[0].message
+
     if 'function_call' in response:
         try:
             return json.loads(response['function_call']['arguments'])['ideas']
