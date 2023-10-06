f2f9a79a6c4898389e324d4edf351cf8a97eb618
Milos Svana <milos.svana@gmail.com>
2023-10-06 10:30:23 +0300

    Migrate ideas endpoint to quart

--- tests/test_ideas.py
+++ tests/test_ideas.py
@@ -1,23 +1,26 @@
-from base import client, db, TEST_HEADERS
+from base import aiotest, client, db, TEST_HEADERS
 
 assert client, db
 
 
-def test_ideas(client):
+@aiotest
+async def test_ideas(client):
     page_contents = {
         'title': 'Ideas',
         'content': 'Sentiment analysis on social media',
         'pageId': '3e47e9f4-c534-4d74-96c9-790b36b1162e'
     }
-    client.get('/refresh-embeddings', headers=TEST_HEADERS)
-    response = client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
+    await client.get('/refresh-embeddings', headers=TEST_HEADERS)
+    response = await client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
     assert response.status_code == 200
-    assert response.json['status'] == 'OK'
-    assert 'ideas' in response.json
-    assert len(response.json['ideas']) > 0
+    json = await response.get_json()
+    assert json['status'] == 'OK'
+    assert 'ideas' in json
+    assert len(json['ideas']) > 0
 
 
-def test_ideas_non_existing_page(client):
+@aiotest
+async def test_ideas_non_existing_page(client):
     """
     Should work too, but there will be no penalization of relatives
     """
@@ -26,15 +29,17 @@ def test_ideas_non_existing_page(client):
         'content': 'Sentiment analysis on social media',
         'pageId': '3e47e9f4-c534-4d74-96c9-790b36b1161f'
     }
-    client.get('/refresh-embeddings', headers=TEST_HEADERS)
-    response = client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
+    await client.get('/refresh-embeddings', headers=TEST_HEADERS)
+    response = await client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
     assert response.status_code == 200
-    assert response.json['status'] == 'OK'
-    assert 'ideas' in response.json
-    assert len(response.json['ideas']) > 0
+    json = await response.get_json()
+    assert json['status'] == 'OK'
+    assert 'ideas' in json
+    assert len(json['ideas']) > 0
 
 
-def test_ideas_long(client):
+@aiotest
+async def test_ideas_long(client):
     with open('tests/data/long_text.md') as long_text_fd:
         content = long_text_fd.read()
     page_contents = {
@@ -42,9 +47,10 @@ def test_ideas_long(client):
         'content': content,
         'pageId': '3e47e9f4-c534-4d74-96c9-790b36b1162e'
     }
-    client.get('/refresh-embeddings', headers=TEST_HEADERS)
-    response = client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
+    await client.get('/refresh-embeddings', headers=TEST_HEADERS)
+    response = await client.post('/ideas/', headers=TEST_HEADERS, json=page_contents)
     assert response.status_code == 200
-    assert response.json['status'] == 'OK'
-    assert 'ideas' in response.json
-    assert len(response.json['ideas']) > 0
+    json = await response.get_json()
+    assert json['status'] == 'OK'
+    assert 'ideas' in json
+    assert len(json['ideas']) > 0
